24401d9adbe2898953f876c0445ed5f17ad0ffd4
Thaise Dantas | Stone <tdantas@stone.com.br>
2017-05-12 19:04:05 -0300

    Funcionalidade para enviar uma mensagem e receber em filas de recebimento diferentes. Criados WorkerQueueSender, WorkerQueueReceiverOne e WorkQueueReceiverTwo

---
+++ WorkerQueueSender/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMqService;
+
+namespace WorkerQueueSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AmqpMessagingService messagingService = new AmqpMessagingService();
+            IConnection connection = messagingService.GetRabbitMqConnection();
+            IModel model = connection.CreateModel();
+            messagingService.SetUpQueueForWorkerQueueDemo(model);
+
+            RunWorkerQueueMessageDemo(model, messagingService);
+            Console.ReadKey();
+
+        }
+
+        private static void RunWorkerQueueMessageDemo(IModel model, AmqpMessagingService messagingService)
+        {
+            Console.WriteLine("Enter your message and press enter. Quit with 'q'.");
+            while (true)
+            {
+                string message = Console.ReadLine();
+                if (message.ToLower() == "q") break;
+                messagingService.SendMessageToWorkerQueue(message, model);
+            }
+        }
+
+
+    }
+}
